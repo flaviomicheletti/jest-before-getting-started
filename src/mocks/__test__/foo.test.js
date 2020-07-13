3a623655df84f512309ad735f19c8c870ffb4230
Flávio Micheletti <flaviomicheletti@hotmail.com>
2020-07-13 15:04:36 -0300

    mock Implementations examples 02

---
+++ src/mocks/__test__/foo.test.js
@@ -0,0 +1,19 @@
+//
+// Example 02 - Mock Implementations
+//
+
+// test.js
+const foo = require('../foo');
+
+jest.mock('../foo'); // this happens automatically with automocking
+
+test('mock', () => {
+    const myMockFn = jest.fn(cb => cb(null, true));
+
+    // foo is a mock function
+    foo.mockImplementation(() => 42);
+    // foo();
+    // > 42
+    expect(foo()).toBe(42);
+
+});
