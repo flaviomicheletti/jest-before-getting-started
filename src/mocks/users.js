48cc31d70ba6190a675b2cdf78d49503f2d3669d
Flávio Micheletti <flaviomicheletti@hotmail.com>
2020-07-25 18:09:16 -0300

    formating

--- src/mocks/users.js
+++ src/mocks/users.js
@@ -1,11 +1,12 @@
+//
 // https://jestjs.io/docs/en/next/mock-functions#mocking-modules
-// users.js
+//
 const axios = require('axios');
 
 class Users {
-  static all() {
-    return axios.get('/users.json').then(resp => resp.data);
-  }
+    static all() {
+        return axios.get('/users.json').then(resp => resp.data);
+    }
 }
 
 module.exports = Users;
