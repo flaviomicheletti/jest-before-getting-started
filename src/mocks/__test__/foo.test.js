bde9e116595441a3dfa9ce7e364796c47a519033
Flávio Micheletti <flaviomicheletti@hotmail.com>
2020-07-25 14:48:42 -0300

    mocks foo

--- src/mocks/__test__/foo.test.js
+++ src/mocks/__test__/foo.test.js
@@ -8,7 +8,9 @@ const foo = require('../foo');
 jest.mock('../foo'); // this happens automatically with automocking
 
 test('mock', () => {
-    const myMockFn = jest.fn(cb => cb(null, true));
+    // const myMockFn = jest.fn(cb => cb(null, true));
+
+    expect(foo()).toBe(undefined);
 
     // foo is a mock function
     foo.mockImplementation(() => 42);
